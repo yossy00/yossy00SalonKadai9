6ca762515a550e997f4c54201e91434db75d0a7b
akio0911 <akio0911@gmail.com>
2021-09-03 16:55:19 +0900

    feedback

--- kadai9/NextViewController.swift
+++ kadai9/NextViewController.swift
@@ -9,34 +9,22 @@ import UIKit
 
 class NextViewController: UIViewController {
 
-    var inputText:String?
-    
+    private(set) var inputText: String?
+
     @IBAction func didPressButton(_ sender: Any) {
-        if(sender as AnyObject).tag == 1{
+        guard let button = sender as? UIButton else { return }
+
+        switch button.tag {
+        case 1:
             inputText = "東京都"
-        }
-        if(sender as AnyObject).tag == 2{
+        case 2:
             inputText = "神奈川県"
-        }
-        if(sender as AnyObject).tag == 3{
+        case 3:
             inputText = "埼玉県"
-        }
-        if(sender as AnyObject).tag == 4{
+        case 4:
             inputText = "千葉県"
+        default:
+            break
         }
     }
- 
-   
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
